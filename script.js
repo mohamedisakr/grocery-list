0e08c1ea76b751ada40e5f111a6ebde187ab6fd2
mohamedisakr <mohamedisakr@gmail.com>
2020-03-30 17:22:30 +0200

    Display Local Storage

--- script.js
+++ script.js
@@ -9,8 +9,10 @@ const clear = document.querySelector(".displayItems-clear");
 
 // event listeners
 submit.addEventListener("click", addItem);
+document.addEventListener("DOMContentLoaded", displayGroceryList);
 
 // functions
+// Add new item
 function addItem(event) {
   event.preventDefault();
   let val = input.value;
@@ -18,7 +20,24 @@ function addItem(event) {
     showAlertAction(addItemsAction, "Please add grocery item");
   } else {
     createItem(val);
+    updateStorage(val);
     showSuccessAction(addItemsAction, `${val} added successfully to the list`);
+  }
+}
+
+// Dispaly list from local storage
+function displayGroceryList() {
+  let groceryList = localStorage.getItem("groceryList")
+    ? JSON.parse(localStorage.getItem("groceryList"))
+    : [];
+  if (groceryList.length === 0) {
+    // create p tage and added to list dom element
+    const paragraph = document.createElement("p");
+    paragraph.textContent = "No items. Please add one";
+    list.appendChild(paragraph);
+  } else {
+    // loop through each item and call createItem function for each item
+    groceryList.forEach(item => createItem(item));
   }
 }
 
@@ -56,4 +75,13 @@ function createItem(val) {
         <i class="far fa-trash-alt"></i>
     </a>`;
   list.appendChild(groceryItem);
+}
+
+// update local storage
+function updateStorage(val) {
+  let groceryList = localStorage.getItem("groceryList")
+    ? JSON.parse(localStorage.getItem("groceryList"))
+    : [];
+  groceryList.push(val);
+  localStorage.setItem("groceryList", JSON.stringify(groceryList));
 }
